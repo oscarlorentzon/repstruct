6722b5eb8512cb4971c23eecb94f6aea8bdf473a
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2014-10-03 09:17:45 -0700

    Improving PCA variable names.

--- analysis/pca.py
+++ analysis/pca.py
@@ -2,17 +2,17 @@ import numpy as np
 
 def neutral_sub_pca(X, neut_factor=0.8):
     
-    x_shape = X.shape
-    row_count = x_shape[0]
-    vector_length = x_shape[1]
+    X_shape = X.shape
+    row_count = X_shape[0]
+    vector_length = X_shape[1]
     
-    # Subtracting a neutral vector for row in X before performing SVD
+    # Subtracting a neutral vector for each row in X before performing SVD.
     N = np.sqrt(1.0/vector_length)*np.array([np.ones(vector_length),]*row_count)
     X_neut = X-neut_factor*N
     
     U,S,V = np.linalg.svd(X_neut)
     
-    # projecting feature vectors on principal components
-    y = np.dot(X_neut, V)
+    # Projecting feature vectors on principal components.
+    Y = np.dot(X_neut, V)
     
-    return y, V
+    return Y, V
