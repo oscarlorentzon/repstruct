92e096b0466746011f302132b032ced4fe71c584
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2014-10-04 02:27:51 -0700

    Loading color training data during feature extraction.

--- features/extractor.py
+++ features/extractor.py
@@ -7,15 +7,22 @@ import features.descriptor as desc
 
 def extract(image_files):
     descriptor_data = scipy.io.loadmat('data/kmeans_descriptor_results.mat')
+    color_data = scipy.io.loadmat('data/kmeans_color_results.mat')
     
     # Descriptor training data cluster centers
     desc_cc = descriptor_data.get('cbest')
-
+    
     # Gets the training data descriptor cluster center indexes and create histogram
-    desc_cc_norm, bins = np.histogram(descriptor_data.get('idxbest'), range(1, 1002))
+    desc_cc_norm, b1 = np.histogram(descriptor_data.get('idxbest'), range(1, desc_cc.shape[0] + 2))
+    
+    color_cc = color_data.get('ccbest')
+    
+    color_cc_norm, b2 = np.histogram(color_data.get('idxcbest'), range(1, color_cc.shape[0] + 2))
+    
+    gaussians = scipy.io.loadmat('data/gaussians.mat')
     
     # Create empty histogram array
-    H = np.array([]).reshape(0, 1000)
+    H = np.array([]).reshape(0, desc_cc.shape[0] + 2*color_cc.shape[0])
     
     # extract descs for all images
     for image_file in image_files:
@@ -33,9 +40,32 @@ def extract(image_files):
         desc_hist = desc.classify(descs, desc_cc)
         
         norm_desc_hist = desc.normalize_by_division(desc_hist, desc_cc_norm)
+        
+        
+        
+        #% Return color in interest points
+        #if any(abs(colorimage(:,1)-colorimage(:,3)) > 0.001)
+        #    [m,n] = size(image);
+        #    load rands
+        #    rx = m*mod((1+rands.x/2.3263)/2,1);
+        #    ry = n*mod((1+rands.y/2.3263)/2,1);
+        #    colors = colorimage((round(locs(:,2))-1)*m + round(locs(:,1)),:);
+        #    rcolors = colorimage(floor(ry)*m + ceil(rx),:);
+        #    
+        #end
 
         H = np.vstack((H, norm_desc_hist))
         
         print "Number of descs", descs.shape[0] #(sum(desc_hist))
         
     return H
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from rsbundler import FlickrRsBundler
 with open ("flickr_key.txt", "r") as myfile: api_key=myfile.readline().rstrip()
 
 bundler = FlickrRsBundler(api_key, 'tajmahal')
-bundler.run()
+bundler.extract()
 
