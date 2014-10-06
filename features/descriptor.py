e77e63b63387c81f997ff2b36fe1575834113a30
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2014-10-06 09:50:02 -0700

    Clustering colors on color training data cluster centers.

--- features/descriptor.py
+++ features/descriptor.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import argmin
 
 def normalize(X):
     """ Normalizes the rows of a 2-D array.
@@ -16,7 +17,7 @@ def normalize(X):
     
     return np.divide(X, np.tile(np.array([norm]).transpose(), [1, X.shape[1]]))
 
-def classify(X, C):
+def classify_cosine(X, C):
     """ Classifies the row vectors of X on the cluster center
         row vectors of C using cosine similarity.
         
@@ -41,11 +42,46 @@ def classify(X, C):
     indices = np.argmax(np.dot(X, C.transpose()), 1)
     
     cc_count = C.shape[0]
-    hist, bins = np.histogram(indices, range(1, cc_count + 2))
+    return np.histogram(indices, range(1, cc_count + 2))[0]
+
+def classify_euclidean(X, C):
+    #% Clusters colours into colour classes using the L2 metric
+    #% size(X) = m x dim
+    #% size(cc) = n x dim
+    #% size(d) = m x n x dim
+
+    m = X.shape[0]
+    dim = X.shape[1]
+    n = C.shape[0]
     
-    return hist
+    # 1263 x 200 x 2
+    X3 = np.zeros((m,n,dim))
+    c3 = np.zeros((m,n,dim))
+    
+    X3 = X.reshape(m, 1, dim).repeat(n, axis=1)
+    
+    c3 = C.reshape(1, n, dim).repeat(m, axis=0)
+    
+    d = np.sum(np.power(X3-c3, 2), axis=2)
 
-
+    a = np.argmin(d, axis=1)  
+    
+    hist = np.histogram(a, range(1, n + 2))[0] 
+    
+    return hist   
+    #    [m,dim] = size(X);
+    #    n = size(cc,1);
+    #    X3 = zeros(m,n,dim);
+    #    c3 = X3;
+    #    X3(:,1,:) = X;
+    #    X3 = X3(:,ones(1,n),:);
+    #    c3(1,:,:) = cc;
+    #    c3 = c3(ones(1,m),:,:);
+    #
+    #    d = sum((X3-c3).^2,3);
+    #
+    #    [meh,idX] = min(d,[],2);
+    
 def normalize_by_division(v, n):
     """ Divides each element of the vector v with the corresponding 
         element of the normalization vector n.
--- features/extractor.py
+++ features/extractor.py
@@ -2,28 +2,30 @@ import scipy.io
 from PIL import Image
 import numpy as np
 
+import matplotlib.colors as mc
+
 import sift as sift
 import features.descriptor as desc
 import display.plothelper as ph
+from features.descriptor import classify_euclidean
 
 def extract(image_files):
     descriptor_data = scipy.io.loadmat('data/kmeans_descriptor_results.mat')
     color_data = scipy.io.loadmat('data/kmeans_color_results.mat')
     
-    # Descriptor training data cluster centers
+    # Get descriptor training data cluster centers and descriptor training data 
+    # cluster center indexes and create histogram.
     desc_cc = descriptor_data.get('cbest')
+    desc_cc_norm = np.histogram(descriptor_data.get('idxbest'), range(1, desc_cc.shape[0] + 2))[0]
     
-    # Gets the training data descriptor cluster center indexes and create histogram
-    desc_cc_norm, b1 = np.histogram(descriptor_data.get('idxbest'), range(1, desc_cc.shape[0] + 2))
     
     color_cc = color_data.get('ccbest')
-    
-    color_cc_norm, b2 = np.histogram(color_data.get('idxcbest'), range(1, color_cc.shape[0] + 2))
+    color_cc_norm = np.histogram(color_data.get('idxcbest'), range(1, color_cc.shape[0] + 2))[0]
     
     # Retrieve Gaussian distributed random points for color retrieval.
     gaussians = scipy.io.loadmat('data/gaussians.mat').get('rands')
-    x = np.mod((1+gaussians['x'][0,0]/2.3263)/2, 1)
-    y = np.mod((1+gaussians['y'][0,0]/2.3263)/2, 1)
+    x = np.mod((1+gaussians['x'][0,0]/2.3263)/2, 1)[:, 0]
+    y = np.mod((1+gaussians['y'][0,0]/2.3263)/2, 1)[:, 0]
     
     # Create empty histogram array
     H = np.array([]).reshape(0, desc_cc.shape[0] + 2*color_cc.shape[0])
@@ -35,32 +37,54 @@ def extract(image_files):
         shape = np.array(image).shape
         
         if len(shape) == 3:
-            image = image.convert('L')
+            gray_image = image.convert('L')
             
-        locs, descs = sift.extract_feature_vectors(image)
+        locs, descs = sift.extract_feature_vectors(gray_image)
         
         descs = desc.normalize(descs)
         desc_cc = desc.normalize(desc_cc)
-        desc_hist = desc.classify(descs, desc_cc)
+        desc_hist = desc.classify_cosine(descs, desc_cc)
         
         norm_desc_hist = desc.normalize_by_division(desc_hist, desc_cc_norm)
         
+        im = np.array(image)
         
+        locs_x = np.empty((locs.shape[0],), dtype='int')
+        locs_y = np.empty((locs.shape[0],), dtype='int')
+        np.round(locs[:, 1], out=locs_x)
+        np.round(locs[:, 0], out=locs_y)
         
-        #% Return color in interest points
-        #if any(abs(colorimage(:,1)-colorimage(:,3)) > 0.001)
-        #    [m,n] = size(image);
-        #    load rands
-        #    rx = m*mod((1+rands.x/2.3263)/2,1);
-        #    ry = n*mod((1+rands.y/2.3263)/2,1);
-        #    colors = colorimage((round(locs(:,2))-1)*m + round(locs(:,1)),:);
-        #    rcolors = colorimage(floor(ry)*m + ceil(rx),:);
-        #    
-        #end
+        colors_desc = im[locs_x, locs_y, :]/255.0
+        
+        rand_x = np.empty((x.shape[0],), dtype='int')
+        rand_y = np.empty((y.shape[0],), dtype='int')
+        im_shape = im.shape
+        np.round(im_shape[0]*np.array(x), out=rand_x)
+        np.round(im_shape[1]*np.array(y), out=rand_y)
+        
+        colors_rand = im[rand_x, rand_y]/255.0
+        
+        a = mc.rgb_to_hsv(colors_desc)
+        b = mc.rgb_to_hsv(colors_rand)
+        
+        acos = np.multiply(a[:,1], np.cos(2*np.pi*a[:,0]))
+        asin = np.multiply(a[:,1], np.sin(2*np.pi*a[:,0]))
+        
+        bcos = np.multiply(b[:,1], np.cos(2*np.pi*b[:,0]))
+        bsin = np.multiply(b[:,1], np.sin(2*np.pi*b[:,0]))
+        
+        #ph.plot_points(acos, asin)
+        #ph.plot_points(bcos, bsin)
+        
+        #ph.plot_points(color_cc[:, 0], color_cc[:, 1])
+        
+        aa = np.vstack((acos, asin)).transpose()
+        
+        classify_euclidean(aa, color_cc)
 
         H = np.vstack((H, norm_desc_hist))
         
-        print "Number of descs", descs.shape[0] #(sum(desc_hist))
+        print "Number of descs", descs.shape[0]
         
     return H
 
