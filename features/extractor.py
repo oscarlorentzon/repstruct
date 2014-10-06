4aeb83fddadd4f412bf0be22b5ef3f23689509a6
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2014-10-06 14:07:54 -0700

    Handling grayscale images color histogram.
    Handling grayscale images color histogram by setting it to the mean of all the histograms of the color images.

--- features/extractor.py
+++ features/extractor.py
@@ -27,7 +27,8 @@ def extract(image_files):
     y = np.mod((1+gaussians['y'][0,0]/2.3263)/2, 1)[:, 0]
     
     # Create empty histogram array
-    H = np.array([]).reshape(0, desc_cc.shape[0] + 2*color_cc.shape[0])
+    H = np.array([]).reshape(0, desc_cc.shape[0])
+    C = np.array([]).reshape(0, 2*color_cc.shape[0])
     
     # extract descriptors and colors for all images
     for image_file in image_files:
@@ -46,63 +47,81 @@ def extract(image_files):
         
         norm_desc_hist = desc.normalize_by_division(desc_hist, desc_cc_norm)
         
-        im = np.array(image)
-        
-        #if len(shape) == 3:
-        locs_x = np.empty((locs.shape[0],), dtype='int')
-        locs_y = np.empty((locs.shape[0],), dtype='int')
-        np.floor(locs[:, 1], out=locs_x)
-        np.floor(locs[:, 0], out=locs_y)
-        colors_desc = im[locs_x, locs_y]/255.0
-        
-        rand_x = np.empty((x.shape[0],), dtype='int')
-        rand_y = np.empty((y.shape[0],), dtype='int')
-        im_shape = im.shape
-        np.floor(im_shape[0]*np.array(x), out=rand_x)
-        np.floor(im_shape[1]*np.array(y), out=rand_y)
-        
-        colors_rand = im[rand_x, rand_y]/255.0
-        
-        a = mc.rgb_to_hsv(colors_desc)
-        b = mc.rgb_to_hsv(colors_rand)
-        
-        acos = np.multiply(a[:,1], np.cos(2*np.pi*a[:,0]))
-        asin = np.multiply(a[:,1], np.sin(2*np.pi*a[:,0]))
-        
-        bcos = np.multiply(b[:,1], np.cos(2*np.pi*b[:,0]))
-        bsin = np.multiply(b[:,1], np.sin(2*np.pi*b[:,0]))
-        
-        #ph.plot_points(acos, asin)
-        #ph.plot_points(bcos, bsin)
-        
-        #ph.plot_points(color_cc[:, 0], color_cc[:, 1])
-        
-        aa = np.vstack((acos, asin)).transpose()
-        bb = np.vstack((bcos, bsin)).transpose()
-        
-        aahist = desc.classify_euclidean(aa, color_cc)
-        bbhist = desc.classify_euclidean(bb, color_cc)
-        
-        aanormhist = desc.normalize_by_division(aahist, color_cc_norm)
-        bbnormhist = desc.normalize_by_division(bbhist, color_cc_norm)
-        #else:
-        #    colors_desc = np.zeros((locs.shape[0], 3))
-        
-
-        w = 0.725
-        cw = (1-w)/2
-        
-        vvv = np.hstack((np.sqrt(w)*norm_desc_hist, np.sqrt(cw)*aanormhist, np.sqrt(cw)*bbnormhist))
-        
-        vvva = np.linalg.norm(vvv)
-
-        H = np.vstack((H, vvv))
+        H = np.vstack((H, norm_desc_hist))
         
         print "Number of descs", descs.shape[0]
         
+        im = np.array(image)
+        
+        if len(shape) == 3:
+            locs_x = np.empty((locs.shape[0],), dtype='int')
+            locs_y = np.empty((locs.shape[0],), dtype='int')
+            np.floor(locs[:, 1], out=locs_x)
+            np.floor(locs[:, 0], out=locs_y)
+            colors_desc = im[locs_x, locs_y]/255.0
+            
+            rand_x = np.empty((x.shape[0],), dtype='int')
+            rand_y = np.empty((y.shape[0],), dtype='int')
+            im_shape = im.shape
+            np.floor(im_shape[0]*np.array(x), out=rand_x)
+            np.floor(im_shape[1]*np.array(y), out=rand_y)
+            
+            colors_rand = im[rand_x, rand_y]/255.0
+            
+            a = mc.rgb_to_hsv(colors_desc)
+            b = mc.rgb_to_hsv(colors_rand)
+            
+            acos = np.multiply(a[:,1], np.cos(2*np.pi*a[:,0]))
+            asin = np.multiply(a[:,1], np.sin(2*np.pi*a[:,0]))
+            
+            bcos = np.multiply(b[:,1], np.cos(2*np.pi*b[:,0]))
+            bsin = np.multiply(b[:,1], np.sin(2*np.pi*b[:,0]))
+            
+            #ph.plot_points(acos, asin)
+            #ph.plot_points(bcos, bsin)
+            
+            #ph.plot_points(color_cc[:, 0], color_cc[:, 1])
+            
+            aa = np.vstack((acos, asin)).transpose()
+            bb = np.vstack((bcos, bsin)).transpose()
+            
+            aahist = desc.classify_euclidean(aa, color_cc)
+            bbhist = desc.classify_euclidean(bb, color_cc)
+            
+            aanormhist = desc.normalize_by_division(aahist, color_cc_norm)
+            bbnormhist = desc.normalize_by_division(bbhist, color_cc_norm)
+        else:
+            aanormhist = np.empty((1, color_cc.shape[0]))
+            aanormhist[:] = np.NAN
+            bbnormhist = np.empty((1, color_cc.shape[0]))
+            bbnormhist[:] = np.NAN
+        
+        C = np.vstack((C, np.hstack((aanormhist,bbnormhist))))
+        
+    
+    nnn = np.linalg.norm(C, axis=1)
+    
+    uuu = ~np.isnan(nnn)
+    
+    CCC = C[uuu, :]
+    
+    mmm = np.mean(CCC, axis=0)
+    
+    mmm = np.sqrt(2) * mmm / np.linalg.norm(mmm)
+    
+    iii = np.isnan(nnn)
+    
+    C[iii, :] = np.tile(mmm, (sum(np.isnan(nnn)), 1))
+
+    w = 0.725
+    cw = (1-w)/2   
+    vvv = np.hstack((np.sqrt(w)*H, np.sqrt(cw)*C))
+    
+    oooo = np.linalg.norm(C, axis=1)
+        
     N = create_neutral_vector(np.array([[desc_cc.shape[0], np.sqrt(w)],[color_cc.shape[0], np.sqrt(cw)],[color_cc.shape[0], np.sqrt(cw)]]), H.shape[0])
             
-    return H, N
+    return vvv, N
 
 def create_neutral_vector(D, rows):
     
--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from rsbundler import FlickrRsBundler
 with open ("flickr_key.txt", "r") as myfile: api_key=myfile.readline().rstrip()
 
 bundler = FlickrRsBundler(api_key, 'statueofliberty')
-bundler.run()
+bundler.extract()
+bundler.process()
+bundler.plot()
 
