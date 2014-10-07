09cb78c12d89bb00439c169783d38ebae657dbf7
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2014-10-07 13:44:29 -0700

    Saving histogram arrays in image tag directory.

--- rsbundler.py
+++ rsbundler.py
@@ -10,13 +10,13 @@ from features.extractor import FeatureMode
 
 class FlickrRsBundler:
     
-    hist_file = "deschists_{0}.txt"
-    neut_file = "neut_{0}.txt"
+    hist_file = "{0}_deschists.txt"
+    neut_file = "{0}_neut.txt"
     
     def __init__(self, api_key, tag, neut_factor=0.8):
         self.flickrWrapper = FlickrWrapper(api_key)
         self.tag = tag
-        self.image_dir = op.dirname(op.abspath(__file__)) + "/images/" + self.tag
+        self.image_dir = op.dirname(op.abspath(__file__)) + "/images/" + self.tag + "/"
         self.neut_factor = neut_factor
         
     def run(self):
@@ -30,20 +30,20 @@ class FlickrRsBundler:
         self.flickrWrapper.download(self.image_dir, self.tag)
         
     def files(self):
-        self.image_files = [op.join(self.image_dir,f) for f in listdir(self.image_dir) if op.isfile(op.join(self.image_dir,f))]
+        self.image_files = [op.join(self.image_dir,f) for f in listdir(self.image_dir) if op.isfile(op.join(self.image_dir,f)) and f.endswith(".jpg")]
     
     def extract(self):
         self.files()
         self.H, self.N = extractor.extract(self.image_files, FeatureMode.Descriptors)
         
     def save(self):
-        np.savetxt(self.hist_file.format(self.tag), self.H)
-        np.savetxt(self.neut_file.format(self.tag), self.N)
+        np.savetxt(self.image_dir + self.hist_file.format(self.tag), self.H)
+        np.savetxt(self.image_dir + self.neut_file.format(self.tag), self.N)
         
     def load(self):
         self.files()
-        self.H = np.loadtxt(self.hist_file.format(self.tag), float)
-        self.N = np.loadtxt(self.neut_file.format(self.tag), float)
+        self.H = np.loadtxt(self.image_dir + self.hist_file.format(self.tag), float)
+        self.N = np.loadtxt(self.image_dir + self.neut_file.format(self.tag), float)
         
     def process(self):        
         #self.Y, V = pca.neutral_sub_pca(self.H)
