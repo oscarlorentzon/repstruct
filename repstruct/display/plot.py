Correct pca plot axes and scaling line based on images.
Adding unit tests for plot pca images.

repstruct/display/plot.py
@@ -61,17 +61,18 @@ def plot_pca_images(image_dir, images, pc_projections, pc1, pc2, im_dim=100, dim
     """
 
     unit = dim / 2
+    line = im_dim / 10.
 
     center = int(np.round(unit * np.max([np.max(np.abs(pc_projections[:, [pc1, pc2]])), min_axis]))) + im_dim
     background_dim = 2 * center
     background = 255 * np.ones((background_dim, background_dim, 3), np.uint8)
-    background[center-2:center+3, :, :] = np.zeros((5, background_dim, 3))
-    background[:, center-2:center+3, :] = np.zeros((background_dim, 5, 3))
+    background[center-line/2:center+line/2, :, :] = np.zeros((line, background_dim, 3))
+    background[:, center-line/2:center+line/2, :] = np.zeros((background_dim, line, 3))
 
     for index, image in enumerate(images):
         im = load_image(image, image_dir, im_dim)
-        row = center - np.round(unit * pc_projections[index, pc1])
-        col = center + np.round(unit * pc_projections[index, pc2])
+        row = center - np.round(unit * pc_projections[index, pc2])
+        col = center + np.round(unit * pc_projections[index, pc1])
         insert_image(background, im, col, row)
 
     fig = pl.figure(figsize=(12, 12))

tests/test_plot.py
@@ -1,8 +1,7 @@
 import unittest
-import math
 import numpy as np
 
-from mock import patch
+from mock import patch, Mock
 
 import repstruct.display.plot as plot
 
@@ -17,6 +16,18 @@ class TestPlot(unittest.TestCase):
     def tearDown(self):
         pass
 
+    @staticmethod
+    def __resize_side_effect(im, dsize):
+        return np.zeros((dsize[0], dsize[1], 3), np.uint8)
+
+    def __count_zero_pixels(self, im):
+        count = 0
+        for row in im:
+            for pixel in row:
+                count = count if np.any(pixel) else count + 1
+
+        return count
+
     @patch('cv2.imwrite')
     @patch('cv2.resize')
     @patch('cv2.imread')
@@ -27,20 +38,14 @@ class TestPlot(unittest.TestCase):
         representative = 1
 
         imread_mock.return_value = np.zeros((im_dim, im_dim, 3), np.uint8)
-
-        def side_effect(im, dsize):
-            return np.zeros((dsize[0], dsize[1], 3), np.uint8)
-
-        resize_mock.side_effect = side_effect
+        resize_mock.side_effect = self.__resize_side_effect
 
         images = np.array([str(i) for i in range(0, image_count)])
         index_closest = np.array(range(0, closest))
         index_representative = np.array(range(0, representative))
 
-        with patch('os.path.join') as join_mock:
-            join_mock.return_value = 'file'
-            plot.plot_representative('dir', images, index_closest, index_representative,
-                                     im_dim=im_dim, cols=1, save_path='path')
+        plot.plot_representative('dir', images, index_closest, index_representative,
+                                 im_dim=im_dim, cols=1, save_path='path')
 
         im_result = imwrite_mock.call_args[0][1]
 
@@ -52,11 +57,7 @@ class TestPlot(unittest.TestCase):
 
         self.assertEqual(width, im_result.shape[1])
 
-        count = 0
-        for row in im_result:
-            for pixel in row:
-                count = count if np.any(pixel) else count + 1
-
+        count = self.__count_zero_pixels(im_result)
         space_count = 2 * im_dim * space
         im_count = count - space_count
 
@@ -73,20 +74,14 @@ class TestPlot(unittest.TestCase):
         representative = 3
 
         imread_mock.return_value = np.zeros((im_dim, im_dim, 3), np.uint8)
-
-        def side_effect(im, dsize):
-            return np.zeros((dsize[0], dsize[1], 3), np.uint8)
-
-        resize_mock.side_effect = side_effect
+        resize_mock.side_effect = self.__resize_side_effect
 
         images = np.array([str(i) for i in range(0, image_count)])
         index_closest = np.array(range(0, closest))
         index_representative = np.array(range(0, representative))
 
-        with patch('os.path.join') as join_mock:
-            join_mock.return_value = 'file'
-            plot.plot_representative('dir', images, index_closest, index_representative,
-                                     im_dim=im_dim, cols=cols, save_path='path')
+        plot.plot_representative('dir', images, index_closest, index_representative,
+                                 im_dim=im_dim, cols=cols, save_path='path')
 
         im_result = imwrite_mock.call_args[0][1]
 
@@ -98,11 +93,7 @@ class TestPlot(unittest.TestCase):
 
         self.assertEqual(width, im_result.shape[1])
 
-        count = 0
-        for row in im_result:
-            for pixel in row:
-                count = count if np.any(pixel) else count + 1
-
+        count = self.__count_zero_pixels(im_result)
         space_count = 2 * (width - 2 * border) * space
         im_count = count - space_count
 
@@ -118,20 +109,14 @@ class TestPlot(unittest.TestCase):
         im_dim = 10
 
         imread_mock.return_value = np.zeros((im_dim, im_dim, 3), np.uint8)
-
-        def side_effect(im, dsize):
-            return np.zeros((dsize[0], dsize[1], 3), np.uint8)
-
-        resize_mock.side_effect = side_effect
+        resize_mock.side_effect = self.__resize_side_effect
 
         images = np.array([str(i) for i in range(0, 3)])
         index_closest = np.array(range(0, 2))
         index_representative = np.array(range(0, 1))
 
-        with patch('os.path.join') as join_mock:
-            join_mock.return_value = 'file'
-            plot.plot_representative('dir', images, index_closest, index_representative,
-                                     im_dim=im_dim, cols=1)
+        plot.plot_representative('dir', images, index_closest, index_representative,
+                                 im_dim=im_dim, cols=1)
 
         self.assertEqual(1, imshow_mock.call_count)
         self.assertEqual(1, figure_mock.call_count)
@@ -147,18 +132,12 @@ class TestPlot(unittest.TestCase):
         structures = np.array([[0, 1], [2, 3], [4]])
 
         imread_mock.return_value = np.zeros((im_dim, im_dim, 3), np.uint8)
-
-        def side_effect(im, dsize):
-            return np.zeros((dsize[0], dsize[1], 3), np.uint8)
-
-        resize_mock.side_effect = side_effect
+        resize_mock.side_effect = self.__resize_side_effect
 
         images = np.array([str(i) for i in range(0, image_count)])
 
-        with patch('os.path.join') as join_mock:
-            join_mock.return_value = 'file'
-            plot.plot_structures('dir', images, structures,
-                                 im_dim=im_dim, cols=1, save_path='path')
+        plot.plot_structures('dir', images, structures,
+                             im_dim=im_dim, cols=1, save_path='path')
 
         im_result = imwrite_mock.call_args[0][1]
 
@@ -170,11 +149,7 @@ class TestPlot(unittest.TestCase):
 
         self.assertEqual(width, im_result.shape[1])
 
-        count = 0
-        for row in im_result:
-            for pixel in row:
-                count = count if np.any(pixel) else count + 1
-
+        count = self.__count_zero_pixels(im_result)
         space_count = (structures.shape[0] - 1) * im_dim * space
         im_count = count - space_count
 
@@ -184,24 +159,18 @@ class TestPlot(unittest.TestCase):
     @patch('cv2.resize')
     @patch('cv2.imread')
     def testPlotStructuresMultipleCols(self, imread_mock, resize_mock, imwrite_mock):
-        im_dim = 100
+        im_dim = 20
         cols = 2
         image_count = 5
         structures = np.array([[0, 1], [2, 3], [4]])
 
         imread_mock.return_value = np.zeros((im_dim, im_dim, 3), np.uint8)
-
-        def side_effect(im, dsize):
-            return np.zeros((dsize[0], dsize[1], 3), np.uint8)
-
-        resize_mock.side_effect = side_effect
+        resize_mock.side_effect = self.__resize_side_effect
 
         images = np.array([str(i) for i in range(0, image_count)])
 
-        with patch('os.path.join') as join_mock:
-            join_mock.return_value = 'file'
-            plot.plot_structures('dir', images, structures,
-                                 im_dim=im_dim, cols=cols, save_path='path')
+        plot.plot_structures('dir', images, structures,
+                             im_dim=im_dim, cols=cols, save_path='path')
 
         im_result = imwrite_mock.call_args[0][1]
 
@@ -213,11 +182,7 @@ class TestPlot(unittest.TestCase):
 
         self.assertEqual(width, im_result.shape[1])
 
-        count = 0
-        for row in im_result:
-            for pixel in row:
-                count = count if np.any(pixel) else count + 1
-
+        count = self.__count_zero_pixels(im_result)
         space_count = (structures.shape[0] - 1) * (width - 2 * border) * space
         im_count = count - space_count
 
@@ -235,23 +200,120 @@ class TestPlot(unittest.TestCase):
         structures = np.array([[0, 1], [2, 3], [4]])
 
         imread_mock.return_value = np.zeros((im_dim, im_dim, 3), np.uint8)
-
-        def side_effect(im, dsize):
-            return np.zeros((dsize[0], dsize[1], 3), np.uint8)
-
-        resize_mock.side_effect = side_effect
+        resize_mock.side_effect = self.__resize_side_effect
 
         images = np.array([str(i) for i in range(0, image_count)])
 
-        with patch('os.path.join') as join_mock:
-            join_mock.return_value = 'file'
-            plot.plot_structures('dir', images, structures,
-                                 im_dim=im_dim, cols=1)
+        plot.plot_structures('dir', images, structures,
+                             im_dim=im_dim, cols=1)
 
         self.assertEqual(1, imshow_mock.call_count)
         self.assertEqual(1, figure_mock.call_count)
         self.assertEqual(1, axis_mock.call_count)
         self.assertEqual(1, show_mock.call_count)
+
+    @patch('matplotlib.pyplot.tick_params')
+    @patch('matplotlib.pyplot.show')
+    @patch('matplotlib.pyplot.imshow')
+    @patch('matplotlib.pyplot.figure')
+    @patch('cv2.resize')
+    @patch('cv2.imread')
+    def testPlotPcaImages(self, imread_mock, resize_mock,
+                          figure_mock, imshow_mock, show_mock, tick_mock):
+        im_dim = 10
+        dim = 100
+        min_axis = 1.
+        image_count = 2
+
+        images = np.array([str(i) for i in range(0, image_count)])
+        pc_projections = np.array([[.5, .0], [.0, .5]])
+
+        imread_mock.return_value = np.zeros((im_dim, im_dim, 3), np.uint8)
+        resize_mock.side_effect = self.__resize_side_effect
+
+        plot.plot_pca_images('dir', images, pc_projections, 0, 1,
+                             im_dim=im_dim, dim=dim, min_axis=min_axis)
+
+        im_result = imshow_mock.call_args[0][0]
+
+        self.assertEqual(3, len(im_result.shape))
+        self.assertEqual(dim + 2*im_dim, im_result.shape[0])
+        self.assertEqual(dim + 2*im_dim, im_result.shape[1])
+
+        self.assertEqual(1, imshow_mock.call_count)
+        self.assertEqual(1, show_mock.call_count)
+        self.assertEqual(1, tick_mock.call_count)
+
+    @patch('matplotlib.pyplot.tick_params')
+    @patch('matplotlib.pyplot.show')
+    @patch('matplotlib.pyplot.imshow')
+    @patch('matplotlib.pyplot.figure')
+    @patch('cv2.resize')
+    @patch('cv2.imread')
+    def testPlotPcaImagesZeroMinAxis(self, imread_mock, resize_mock,
+                                     figure_mock, imshow_mock, show_mock, tick_mock):
+        im_dim = 10
+        dim = 100
+        min_axis = .0
+        image_count = 2
+
+        images = np.array([str(i) for i in range(0, image_count)])
+        pc_projections = np.array([[.5, .0], [.0, .5]])
+
+        imread_mock.return_value = np.zeros((im_dim, im_dim, 3), np.uint8)
+        resize_mock.side_effect = self.__resize_side_effect
+
+        plot.plot_pca_images('dir', images, pc_projections, 0, 1,
+                             im_dim=im_dim, dim=dim, min_axis=min_axis)
+
+        im_result = imshow_mock.call_args[0][0]
+
+        self.assertEqual(3, len(im_result.shape))
+        self.assertEqual(dim/2 + 2*im_dim, im_result.shape[0])
+        self.assertEqual(dim/2 + 2*im_dim, im_result.shape[1])
+
+        self.assertEqual(1, imshow_mock.call_count)
+        self.assertEqual(1, show_mock.call_count)
+        self.assertEqual(1, tick_mock.call_count)
+
+    @patch('matplotlib.pyplot.yticks')
+    @patch('matplotlib.pyplot.xticks')
+    @patch('matplotlib.pyplot.close')
+    @patch('matplotlib.pyplot.imshow')
+    @patch('matplotlib.pyplot.figure')
+    @patch('cv2.resize')
+    @patch('cv2.imread')
+    def testPlotPcaImagesSaveFig(self, imread_mock, resize_mock,
+                                     figure_mock, imshow_mock, close_mock,
+                                     xticks_mock, yticks_mock):
+        im_dim = 10
+        dim = 100
+        min_axis = .0
+        image_count = 2
+
+        images = np.array([str(i) for i in range(0, image_count)])
+        pc_projections = np.array([[.5, .0], [.0, .5]])
+
+        imread_mock.return_value = np.zeros((im_dim, im_dim, 3), np.uint8)
+        resize_mock.side_effect = self.__resize_side_effect
+        figure_instance = figure_mock.return_value
+        figure_instance.savefig = Mock()
+
+        plot.plot_pca_images('dir', images, pc_projections, 0, 1,
+                             im_dim=im_dim, dim=dim, min_axis=min_axis,
+                             ticks=True, save_path='path')
+
+        im_result = imshow_mock.call_args[0][0]
+
+        self.assertEqual(3, len(im_result.shape))
+        self.assertEqual(dim/2 + 2*im_dim, im_result.shape[0])
+        self.assertEqual(dim/2 + 2*im_dim, im_result.shape[1])
+
+        self.assertEqual(1, figure_instance.savefig.call_count)
+        self.assertEqual(1, imshow_mock.call_count)
+        self.assertEqual(1, close_mock.call_count)
+        self.assertEqual(1, xticks_mock.call_count)
+        self.assertEqual(1, yticks_mock.call_count)
 
 
 if __name__ == '__main__':